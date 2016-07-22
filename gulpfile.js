feb827a235984b43af004743c3ec9df9e048376d
Andres Zapata <me@azapata.io>
2016-07-22 15:30:13 -0500

    Add setup gulplfile, add react and babel

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,32 @@ var elixir = require('laravel-elixir');
  |
  */
 
+/*
+ * Bower Root
+ */
+var bower = 'resources/assets/bower/';
+
+/*
+ * Assets Root
+ */
+var root = 'resources/assets/';
+
+/*
+ * Utilities Directory
+ */
+var utils = root + 'js/utils/';
+
+var socketio = 'public/vendor/socketio/';
+
+/*
+ * Assets Output
+ */
+var css = 'public/assets/css/';
+var js = 'public/assets/js/';
+
 elixir(function(mix) {
     mix.sass('app.scss');
+
+    mix.copy(bower + 'socket.io.client', socketio);
+
 });
